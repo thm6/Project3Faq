26b7e69f4a995463154828bfe0682cfbd810d7a1
Tai <thm6@njit.edu>
2019-04-19 22:36:51 -0400

    Added Answer migration, factory, model and seeder, Answer test.

--- app/Answer.php
+++ app/Answer.php
@@ -4,17 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Question extends Model
+class Answer extends Model
 {
+    protected $fillable = ['body'];
+
+
     public function user()
     {
         return $this->belongsTo('App\User');
     }
-
-
-    public function answers()
+    public function question()
     {
-        return $this->hasMany('App\Answer');
+        return $this->belongsTo('App\Question');
     }
-
 }
--- app/User.php
+++ app/User.php
@@ -73,7 +73,10 @@ class User extends Authenticatable
         return $this->hasMany('App\Question');
     }
 
-
+    public function answers()
+    {
+        return $this->hasMany('App\Answer');
+    }
 
 
 }
