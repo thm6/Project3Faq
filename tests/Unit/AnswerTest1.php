f4dc0b2dbb049d36870fa42ce4a028038d14a699
Tai <thm6@njit.edu>
2019-04-21 02:12:05 -0400

    testing

--- tests/Unit/AnswerTest1.php
+++ tests/Unit/AnswerTest1.php
@@ -6,7 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class AnswerTest extends TestCase
+class AnswerTest1 extends TestCase
 {
     /**
      * A basic unit test example.
